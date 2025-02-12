3ae8adbbee0ed9f276bb0957227377b7fc0188e4
Abdullah Ansari <abdullahansari018@gmail.com>
2025-02-12 22:59:59 +0530

    Add HomeView

---
+++ SwiftfulSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Abdullah Ansari on 12/02/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2!")
+    }
+}
+
+#Preview {
+    HomeView()
+}
