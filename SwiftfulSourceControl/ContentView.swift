847ed641352208d3a541565625e80335b2427e48
Abdullah Ansari <abdullahansari018@gmail.com>
2025-02-12 23:15:49 +0530

    [Clean] Add commit message documentation.

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -7,6 +7,27 @@
 
 import SwiftUI
 
+/*
+ 
+ COMMIT MESSAGE
+ 
+ NEW FEATURE
+ [Feature] Description of the feature
+ 
+ BUG IN PRODUCTION
+ [Patch] Description of patch
+ 
+ BUG NOT IN PRODUCTION
+ [Bug] Description of the bug
+ 
+ MUNDANE TASKS:
+ [Clean] Description of the changes
+ 
+ RELEASE:
+ [Release] Description of the release
+ 
+ */
+
 struct ContentView: View {
     var body: some View {
         VStack {
